40b365973f1302d8dc91e9c7dcee70bdcdbe36f3
SyntaxError9886 <73121106+SyntaxError9886@users.noreply.github.com>
2022-08-16 21:54:41 -0400

    Added a new line for testing GitHub -RS

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello Wrld");
+        System.out.println("This is a test for Github change");
+
+        mathMethods math = new mathMethods();
+
+        int var = math.add(5,6);
+        System.out.println(var);
+
+        int var2 = math.multiply(5,6);
+        System.out.println(var2);
+
+        int var3 = math.subtract(5,6);
+        System.out.println(var3);
+
+    }
+}
---
+++ src/mathMethods.java
@@ -0,0 +1,23 @@
+public class mathMethods {
+
+    public int add(int num1, int num2){
+        int answer = num1 + num2;
+        return answer;
+    }
+
+    public int subtract(int num1, int num2){
+        int answer = num1 - num2;
+        return answer;
+    }
+
+    public int multiply(int num1, int num2){
+        int answer = num1 * num2;
+        return answer;
+    }
+
+    public double divide(double num1, double num2){
+        double answer = num1/num2;
+        return answer;
+    }
+
+}
